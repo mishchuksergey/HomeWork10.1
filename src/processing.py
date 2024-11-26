1fa5ef528bb5b25815d99a527f5d1a3a353fffe8
mishchuk <mishchuksg@gmail.com>
2024-11-26 22:54:25 +0300

    add functions

---
+++ src/processing.py
@@ -0,0 +1,17 @@
+def filter_by_state(dictionary: dict,  state: str) -> dict:
+    """Функция,которая возвращает новый список словарей, содержащий только те словари,
+       у которых ключ state соответствует указанному значению"""
+    new_dictionary = [{}]
+    for date in dictionary:
+        if date['state'] == state:
+            new_dictionary.append(date)
+    return new_dictionary
+
+
+
+def sort_by_date(dictionary: dict) -> dict:
+    """Функция, которая возвращает новый список, отсортированный по дате"""
+    if ascending == False:
+        return sorted(dictionary, key=lambda ascending: ascending['date'], reverse=True)
+    else:
+        return sorted(dictionary, key=lambda ascending: ascending['date'])
